95f52e73970f6c3d74779aabeea324cf534c2e4e
jsjang-ai <jsjang.ai@gmail.com>
2021-06-16 21:13:35 +0900

    03 리액트 기초 개념 알아보기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({ love }) {
+  return <h1>I like {love}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food love="jjajangmien" />
+      <Food love="ramen" />
+      <Food love="samgiopsal" />
     </div>
   );
 }
